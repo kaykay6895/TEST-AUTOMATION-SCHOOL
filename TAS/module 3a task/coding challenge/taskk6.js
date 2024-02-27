ef92f107e5037447c83c0786ad2f228c888f7ef4
kaykay6895 <kayodearemu76@gmail.com>
2024-02-27 22:36:40 +0000

    TAS/module 3a task/coding challenge/taskk6.js

---
+++ TAS/module 3a task/coding challenge/taskk6.js
@@ -0,0 +1,9 @@
+// Array of strings to be sorted
+
+const stringsArray = ["benz", "toyota", "bmw", "volvo", "acura"];
+
+// Sort the array of strings in alphabetical order
+stringsArray.sort();
+
+// Print the sorted array
+console.log("Sorted array of strings:", stringsArray);
