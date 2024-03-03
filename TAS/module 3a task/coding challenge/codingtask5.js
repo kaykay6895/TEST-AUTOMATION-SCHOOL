f24a28358b1675f01982a181c8451fedcdd217d3
kaykay6895 <kayodearemu76@gmail.com>
2024-03-03 08:16:09 +0000

    new file:   TAS/module 3a task/coding challenge/codingtask5.js

---
+++ TAS/module 3a task/coding challenge/codingtask5.js
@@ -0,0 +1,7 @@
+function reverseArray(array) {
+    return array.reverse();
+  }
+  
+  const originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const reversedArray = reverseArray(originalArray);
+  console.log(reversedArray); 
