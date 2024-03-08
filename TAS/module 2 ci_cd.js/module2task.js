b9acfab52b5e6b64c722a38dc9c58bacc4436944
kaykay6895 <kayodearemu76@gmail.com>
2024-03-08 17:16:20 +0000

    modified:   TAS/module 2 ci_cd.js/module2task.js

--- TAS/module 2 ci_cd.js/module2task.js
+++ TAS/module 2 ci_cd.js/module2task.js
@@ -3,47 +3,6 @@
 
 //console.log('firstName + surName');
 
-//console.log('Math.round(2.2');
-
-//let calculation = 2 + 2;
-//console.log(calculation)
-
-//console.log(calculation + 2)
-
-//let highincome = true;
-//let goodcreditscore = true;
-//let eligibleforloan = highincome && goodcreditscore
-
-//console.log(eligibleforloan)
-
-
-//let x = 10;
-
-//let y = (x > 5) && (x < 15); 
-
-//console.log(x&&y)
-
-
-//let x = 1; 
-
-//let y = x !== 2;
-//console.log(x&&y)
-
-//let a = 'red'
-//let b = 'green'
-
-//console.log(a&&b)
-
-//if hour is between 6am and 12pm: Good morning
- //if it is between 12pm and 6pm; Good Afternoon
-//otherwise: Good evening 
-
-
-let x =10
-let y =20
-
-console.log(x+y)
-
 
 
 
